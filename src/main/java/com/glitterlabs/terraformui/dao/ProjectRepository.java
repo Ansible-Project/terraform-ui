0095a66fbe0aed4c9a8b381e895b25c58b4d5aed
Restyled.io <commits@restyled.io>
2022-01-02 18:07:26 +0000

    Restyled by clang-format

--- src/main/java/com/glitterlabs/terraformui/dao/ProjectRepository.java
+++ src/main/java/com/glitterlabs/terraformui/dao/ProjectRepository.java
@@ -1,30 +1,28 @@
 package com.glitterlabs.terraformui.dao;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.glitterlabs.terraformui.model.Cloud;
 import com.glitterlabs.terraformui.model.Project;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * The Interface CloudProjectRepository.
  */
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-	/**
-	 * Find by name.
-	 *
-	 * @param name the name
-	 * @return the list of projects
-	 */
-	List<Project> findByName(String name);
+  /**
+   * Find by name.
+   *
+   * @param name the name
+   * @return the list of projects
+   */
+  List<Project> findByName(String name);
 
-	/**
-	 * Find by cloud type.
-	 *
-	 * @param cloud
-	 * @return the list of projects
-	 */
-	List<Project> findByCloudType(Cloud cloud);
+  /**
+   * Find by cloud type.
+   *
+   * @param cloud
+   * @return the list of projects
+   */
+  List<Project> findByCloudType(Cloud cloud);
 }
--- src/main/java/com/glitterlabs/terraformui/service/ResourceService.java
+++ src/main/java/com/glitterlabs/terraformui/service/ResourceService.java
@@ -1,5 +1,10 @@
 package com.glitterlabs.terraformui.service;
 
+import com.glitterlabs.terraformui.dao.ProjectRepository;
+import com.glitterlabs.terraformui.model.Project;
+import com.glitterlabs.terraformui.model.Resource;
+import com.glitterlabs.terraformui.util.DirectoryUtil;
+import com.glitterlabs.terraformui.util.GlobalProperties;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -7,74 +12,73 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.glitterlabs.terraformui.dao.ProjectRepository;
-import com.glitterlabs.terraformui.model.Project;
-import com.glitterlabs.terraformui.model.Resource;
-import com.glitterlabs.terraformui.util.DirectoryUtil;
-import com.glitterlabs.terraformui.util.GlobalProperties;
-
 /**
  * The Class CloudProjectService.
  */
 @Service
 public class ResourceService {
-	private static final Logger LOG = LoggerFactory.getLogger(ResourceService.class);
+  private static final Logger LOG =
+      LoggerFactory.getLogger(ResourceService.class);
 
-	/** The dao. */
-	@Autowired
-	private ProjectRepository dao;
+  /** The dao. */
+  @Autowired private ProjectRepository dao;
 
-	@Autowired
-	private GlobalProperties prop;
+  @Autowired private GlobalProperties prop;
 
-	/**
-	 * Creates the resource.
-	 *
-	 * @param projectId the project id
-	 * @param name the name
-	 * @param content the content
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 */
-	public void create(final String projectId, final String name, final String content, final String resourceType) throws IOException {
-		final Project project = this.dao.getOne(Long.valueOf(projectId));
-		final Path path = Paths.get(this.prop.getDirectoryPath(), project.getPath(), name);
-		if (StringUtils.equalsIgnoreCase(resourceType, "file")) {
-			Files.createFile(path);
-			final FileWriter fileWriter = new FileWriter(path.toFile());
-			fileWriter.write(content);
-			fileWriter.flush();
-			fileWriter.close();
-		} else if (StringUtils.equalsIgnoreCase(resourceType, "directory")) {
-			Files.createDirectory(path);
-		}
-		LOG.debug("Create {}.", path);
-	}
+  /**
+   * Creates the resource.
+   *
+   * @param projectId the project id
+   * @param name the name
+   * @param content the content
+   * @throws IOException Signals that an I/O exception has occurred.
+   */
+  public void create(final String projectId, final String name,
+                     final String content, final String resourceType)
+      throws IOException {
+    final Project project = this.dao.getOne(Long.valueOf(projectId));
+    final Path path =
+        Paths.get(this.prop.getDirectoryPath(), project.getPath(), name);
+    if (StringUtils.equalsIgnoreCase(resourceType, "file")) {
+      Files.createFile(path);
+      final FileWriter fileWriter = new FileWriter(path.toFile());
+      fileWriter.write(content);
+      fileWriter.flush();
+      fileWriter.close();
+    } else if (StringUtils.equalsIgnoreCase(resourceType, "directory")) {
+      Files.createDirectory(path);
+    }
+    LOG.debug("Create {}.", path);
+  }
 
-	public List<Resource> findAllResources(final String projectId, final String subpath) {
-		List<Resource> result = new ArrayList<>();
-		final Project project = this.dao.getOne(Long.valueOf(projectId));
-		if (project != null) {
-			try {
-				result = DirectoryUtil.getResources(Paths.get(this.prop.getDirectoryPath(), project.getPath(), subpath));
-			} catch (final IOException e) {
-				LOG.error("Unable to get resources.", e);
-			}
-		}
-		return result;
-	}
+  public List<Resource> findAllResources(final String projectId,
+                                         final String subpath) {
+    List<Resource> result = new ArrayList<>();
+    final Project project = this.dao.getOne(Long.valueOf(projectId));
+    if (project != null) {
+      try {
+        result = DirectoryUtil.getResources(Paths.get(
+            this.prop.getDirectoryPath(), project.getPath(), subpath));
+      } catch (final IOException e) {
+        LOG.error("Unable to get resources.", e);
+      }
+    }
+    return result;
+  }
 
-	public String getResourceContent(final String projectId, final String resourcePath) throws IOException {
-		final Project project = this.dao.getOne(Long.valueOf(projectId));
-		final Path path = Paths.get(this.prop.getDirectoryPath(), project.getPath(), resourcePath);
-		final byte[] bytes = Files.readAllBytes(path);
-		return new String(bytes);
-	}
-
+  public String getResourceContent(final String projectId,
+                                   final String resourcePath)
+      throws IOException {
+    final Project project = this.dao.getOne(Long.valueOf(projectId));
+    final Path path = Paths.get(this.prop.getDirectoryPath(), project.getPath(),
+                                resourcePath);
+    final byte[] bytes = Files.readAllBytes(path);
+    return new String(bytes);
+  }
 }
